9b90eba034824be96609b4fe9cd6eeeb5c57f7fb
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-05-19 11:28:05 +0200

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,28 @@
+import { render } from '@czechitas/render';
+import '../global.css';
+import './index.css';
+import { getRandomQuote } from './quotes.js';
+import { moveToTop } from './quotes.js';
+
+export const renderQuotes = (quotes) => {
+  quotes.forEach((quote) => {
+    document.body.innerHTML += `<p>${quote}</p>`;
+  });
+};
+
+// Vyberte všechny citáty na stránce
+const allQuotes = document.querySelectorAll('.quote');
+
+// Přidejte posluchač události click na každý citát
+allQuotes.forEach((quote, index) => {
+  quote.addEventListener('click', () => {
+    // Zavolejte funkci moveToTop s indexem citátu
+    moveToTop(index, quote); // Předpokládám, že máte pole citátů s názvem myQuotes
+    // Znovu vykreslete citáty
+    renderQuotes(quote);
+  });
+});
+
+const quoteElement = document.getElementById('root');
+const quote = getRandomQuote();
+quoteElement.innerHTML = `<p>Citát dne: ${quote}</p>`;
---
+++ src/pages/quotes.js
@@ -0,0 +1,35 @@
+import { renderQuotes } from './index.jsx';
+
+const quotes = [
+  'So many books, so little time.',
+  'The greatest wealth is to live content with little.',
+  'A great man is always willing to be little.',
+  'The journey of a thousand miles begins with one step.',
+  'That which does not kill us makes us stronger.',
+  'A room without books is like a body without a soul.',
+  'You only live once, but if you do it right, once is enough.',
+  'Be the change that you wish to see in the world.',
+  'Live as if you were to die tomorrow. Learn as if you were to live forever.',
+  'Always forgive your enemies; nothing annoys them so much.',
+  'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.',
+  'It does not do to dwell on dreams and forget to live.',
+  'Life is what happens to us while we are making other plans.',
+  "I have not failed. I've just found 10,000 ways that won't work.",
+  'I solemnly swear that I am up to no good.',
+];
+
+export const getRandomQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+};
+
+export const moveToTop = (index, quotes) => {
+  if (index <= quotes.length) {
+    const moveQuote = quotes.splice(index, 1)[0];
+    quotes.unshift(moveQuote);
+  }
+  return quotes;
+};
+
+
+renderQuotes(quotes);
